ced03c303cd2b5ae0bd562da960b4337676f7466
Carl Boettiger <cboettig@gmail.com>
2020-10-08 18:15:49 +0000

    fix typo

--- fishing/sb3/fishing_optimaly.py
+++ fishing/sb3/fishing_optimaly.py
@@ -1,29 +1,12 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.6.0
-#   kernelspec:
-#     display_name: 'Python 3.7.6 64-bit (''base'': conda)'
-#     name: python_defaultSpec_1598045789527
-# ---
 
-# # Fishing optimaly
-
-# +
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
-
 import gym 
 import gym_fishing
-# -
+
 
 # ## Continuous action space 
-
 cts_env = gym.make('fishing-v1')
 
 
@@ -42,31 +25,26 @@ def simul(environment) :
 
 out=simul(cts_env)
 
-plt.plot(out[:,1])
-plt.ylabel('state')
-plt.xlabel('time')
-plt.show()
+fig, axs = plt.subplots(3,1)
+axs[0].plot(out[:,0], out[:,1])
+axs[0].set_ylabel('state')
+axs[1].plot(out[:,0], out[:,2])
+axs[1].set_ylabel('action')
+axs[2].plot(out[:,0], out[:,3])
+axs[2].set_ylabel('reward')
 
-plt.plot(out[:,2])
-plt.ylabel('action')
-plt.xlabel('time')
-plt.show()
+fig.tight_layout()
+plt.savefig("results/optimal_v1.png")
 
-plt.plot(out[:,3])
-plt.ylabel('reward')
-plt.xlabel('time')
-plt.show()
+
 
 # ## Discrete Action Space
 
 env = gym.make('fishing-v0')
-
-
 def simulate(environment) :
     time_step = environment.reset()
     episode_return = 0.0
     output = np.zeros(shape = (1000, 4))
-
     action = 0
     time_step = environment.step(action)
     episode_return += time_step[1]
@@ -83,9 +61,8 @@ def simulate(environment) :
         output[it] = (it, time_step[0], action, episode_return)
     return output    
 
-
 time_step = env.reset()
-time_step =env.step(1)
+time_step = env.step(1)
 time_step
 
 out = simulate(env)
@@ -95,12 +72,12 @@ out = simulate(env)
 fig, axs = plt.subplots(3,1)
 axs[0].plot(out[:,0], out[:,1])
 axs[0].set_ylabel('state')
-axs[1].plot((out[:,0], out[:,2])
+axs[1].plot(out[:,0], out[:,2])
 axs[1].set_ylabel('action')
-axs[2].plot((out[:,0], out[:,3])
+axs[2].plot(out[:,0], out[:,3])
 axs[2].set_ylabel('reward')
 
 fig.tight_layout()
-plt.savefig("results/optimal.png")
+plt.savefig("results/optimal_v0.png")
 
 
