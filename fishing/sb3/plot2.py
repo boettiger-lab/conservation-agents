071e688f238cea67d8c71c24af5fb26d85570b19
Carl Boettiger <cboettig@gmail.com>
2020-10-09 22:03:25 +0000

    and sac_4

--- fishing/sb3/plot2.py
+++ fishing/sb3/plot2.py
@@ -4,7 +4,7 @@ from stable_baselines3 import SAC
 import numpy as np
 
 env = gym.make('fishing-v1')
-for i in range(4):
+for i in range(5):
     model = SAC.load(f"models/sb3_sac_"+str(i))
 
     ## simulate and plot results
