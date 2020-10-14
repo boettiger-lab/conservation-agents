89c5b915bd41ad46cc8e3ae0abe4548594a43209
Carl Boettiger <cboettig@gmail.com>
2020-10-14 16:18:20 +0000

    add git log

--- git-log.py
+++ git-log.py
@@ -1,9 +1,4 @@
-import sh
-git = sh.git.bake(_cwd='.')
-git.status()
-# add a file
-print git.add('git-log.py')
-# commit
-print git.commit(m='robot update')
-# now we are one commit ahead
-print git.status()
+import git
+repo = git.Repo(".")
+repo.git.add("git-log.py")
+sha = repo.commit().hexsha
