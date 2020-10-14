bb0d3f72b4af330048eb89ac81c0105b9306f836
Carl Boettiger <cboettig@gmail.com>
2020-10-14 19:51:49 +0000

    leaderboard commits and tracks hashes

--- fishing/sb3/leaderboard.py
+++ fishing/sb3/leaderboard.py
@@ -2,11 +2,10 @@ from datetime import datetime
 from csv import writer
 from git import Repo
 import os
-def leaderboard(agent, env, mean, std, file = "results/leaderboard.csv"):
+def leaderboard(agent, env, mean, std, script, file = "results/leaderboard.csv"):
     
     ## Commit file and compute GitHub URL
     repo = Repo(".", search_parent_directories=True)
-    script = os.path.basename(__file__)
     path = os.path.relpath(script, repo.git.working_dir)
     repo.git.add(path)
     if len(repo.index.diff("HEAD")) > 0:
