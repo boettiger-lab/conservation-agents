49f1aa0ab3432f36fe53200d7e4fa1bcbd6626df
Carl Boettiger <cboettig@gmail.com>
2020-10-14 20:13:46 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_A2C.py
+++ fishing/sb3/fishing_with_A2C.py
@@ -4,8 +4,10 @@ import gym_fishing
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
 from leaderboard import leaderboard
+import os
 
-env = gym.make('fishing-v0', n_actions = 100)
+ENV = "fishing-v0"
+env = gym.make(ENV, n_actions = 100)
 model = A2C('MlpPolicy', env, verbose=0)
 model.learn(total_timesteps=500000)
 
@@ -17,6 +19,6 @@ env.plot(df, "results/a2c.png")
 ## Evaluate model
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
-leaderboard("A2C", mean_reward, std_reward)
+leaderboard("A2C", ENV, mean_reward, std_reward, os.path.basename(__file__))
 
 model.save("models/a2c")
