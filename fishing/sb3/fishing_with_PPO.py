293964c1760790a4a41707f43313fa7eeaff0783
Carl Boettiger <cboettig@gmail.com>
2020-10-14 20:12:43 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_PPO.py
+++ fishing/sb3/fishing_with_PPO.py
@@ -4,8 +4,10 @@ import gym_fishing
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from leaderboard import leaderboard
+import os
 
-env = gym.make('fishing-v0', n_actions = 100)
+ENV = "fishing-v0"
+env = gym.make(ENV, n_actions = 100)
 model = PPO('MlpPolicy', env, verbose=0)
 model.learn(total_timesteps=300000)
 
@@ -17,7 +19,7 @@ env.plot(df, "results/ppo.png")
 ## Evaluate model
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
-leaderboard("PPO", mean_reward, std_reward)
+leaderboard("PPO", ENV, mean_reward, std_reward, os.path.basename(__file__))
 
 
 model.save("models/ppo")
