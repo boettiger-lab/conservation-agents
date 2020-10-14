e8a2c107701f206fb0b2391abd5892b5c6ce4882
Carl Boettiger <cboettig@gmail.com>
2020-10-14 16:42:28 +0000

    'robot commit before running script'

--- fishing/sb3/leaderboard.py
+++ fishing/sb3/leaderboard.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 from csv import writer
+from git import Repo
 
 def leaderboard(agent, mean, std, file = "results/leaderboard.csv"):
+    
+    repo = Repo(".")
+    repo.git.add("git-log.py")
+    repo.git.commit("-m add git log")
+    repo.git.status()
+    sha = repo.commit().hexsha
+    sha
+
+    
     stream = open(file, 'a+')
     now = datetime.now()
     row_contents = [agent,
