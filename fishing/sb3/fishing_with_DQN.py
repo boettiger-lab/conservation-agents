1dbeba1041acf9a3f7ad00d1e56b6b44c802ad0e
Carl Boettiger <cboettig@gmail.com>
2020-10-14 19:52:18 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_DQN.py
+++ fishing/sb3/fishing_with_DQN.py
@@ -4,9 +4,11 @@ import gym_fishing
 from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 from leaderboard import leaderboard
+import os
 
 # Create environment
-env = gym.make('fishing-v0', n_actions=100)
+ENV = "fishing-v0"
+env = gym.make(ENV, n_actions=100)
 # Instantiate the agent
 model = DQN('MlpPolicy', env, verbose=0)
 # Train the agent
@@ -19,7 +21,7 @@ env.plot(df, "results/dqn.png")
 # Evaluate the agent
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
-leaderboard("DQN", mean_reward, std_reward)
+leaderboard("DQN", ENV, mean_reward, std_reward, os.path.basename(__file__))
 
 # Save the agent
 model.save("models/dqn_fish_v0")
