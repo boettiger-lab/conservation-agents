991f3637c447f8e57bdb8ab208494f89b3c182f6
Carl Boettiger <cboettig@gmail.com>
2020-10-15 23:04:05 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_SAC.py
+++ fishing/sb3/fishing_with_SAC.py
@@ -2,9 +2,9 @@ import gym
 import gym_fishing
 from stable_baselines3 import SAC
 from stable_baselines3.common.evaluation import evaluate_policy
-from leaderboard import leaderboard
+import leaderboard
 import os
-url = hash_url(os.path.basename(__file__)) # get hash URL at start of execution
+url = leaderboard.hash_url(os.path.basename(__file__)) # get hash URL at start of execution
 
 
 ENV = "fishing-v2"
@@ -20,7 +20,7 @@ env.plot(df, "results/sac.png")
 ## Evaluate model
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
-leaderboard("SAC", ENV, mean_reward, std_reward, url)
+leaderboard.leaderboard("SAC", ENV, mean_reward, std_reward, url)
 
 
 model.save("models/SAC")
