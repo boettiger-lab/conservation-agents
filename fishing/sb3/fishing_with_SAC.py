3ffa37aabc97a66580833b64df4fe05ee7d0e866
Carl Boettiger <cboettig@gmail.com>
2020-10-16 01:10:10 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_SAC.py
+++ fishing/sb3/fishing_with_SAC.py
@@ -7,8 +7,8 @@ import os
 url = leaderboard.hash_url(os.path.basename(__file__)) # get hash URL at start of execution
 
 
-ENV = "fishing-v2"
-env = gym.make(ENV, C = .01)
+ENV = "fishing-v1"
+env = gym.make(ENV, sigma = 0.02)
 model = SAC('MlpPolicy', env, verbose=0)
 model.learn(total_timesteps=200000)
 
