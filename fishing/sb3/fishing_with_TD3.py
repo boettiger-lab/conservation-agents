78370d2b4109a03e905d92331a91ae5db55c3edd
Carl Boettiger <cboettig@gmail.com>
2020-10-16 18:10:19 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_with_TD3.py
+++ fishing/sb3/fishing_with_TD3.py
@@ -6,7 +6,7 @@ import leaderboard
 import os
 url = leaderboard.hash_url(os.path.basename(__file__)) # get hash URL at start of execution
 
-ENV = "fishing-v3"
+ENV = "fishing-v4"
 env = gym.make(ENV)
 model = TD3('MlpPolicy', env, verbose=0)
 model.learn(total_timesteps=300000)
