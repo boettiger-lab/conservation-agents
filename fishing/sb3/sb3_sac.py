708a95edfe742e39e0dbdb69b68cd9a893a4ca9d
Carl Boettiger <cboettig@gmail.com>
2020-10-23 22:31:11 +0000

    'robot commit before running script'

--- fishing/sb3/sb3_sac.py
+++ fishing/sb3/sb3_sac.py
@@ -1,7 +1,5 @@
 import os
 import sys
-sys.path.append(os.path.realpath('../..'))
-
 import gym
 from gym import spaces
 import gym_fishing
@@ -11,6 +9,10 @@ import torch as th
 from stable_baselines3.sac.policies import SACPolicy
 from stable_baselines3 import SAC
 from stable_baselines3.common.noise import NormalActionNoise
+
+import leaderboard
+url = leaderboard.hash_url(os.path.basename(__file__)) # get hash URL at start of execution
+
 
 env = gym.make('fishing-v1')
 policy_kwargs = dict(activation_fn=th.nn.ReLU, 
@@ -28,6 +30,18 @@ model = SAC("MlpPolicy",
             gamma=0.995, 
             policy_kwargs=policy_kwargs)
 model.learn(total_timesteps=int(1e6), log_interval=int(1e3))
-model.save("sb3_sac")
+model.save("tuned-sac")
+
+
+## simulate and plot results
+df = env.simulate(model, reps=10)
+env.plot(df, "results/tuned-sac.png")
+
+
+## Evaluate model
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
+print("mean reward:", mean_reward, "std:", std_reward)
+leaderboard.leaderboard("SAC-tuned", ENV, mean_reward, std_reward, url)
+
 
 
