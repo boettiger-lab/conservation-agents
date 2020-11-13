7532860ea4da7f913f58a3b82c2e198a3dcda432
Carl <cboettig@gmail.com>
2020-11-13 00:33:53 +0000

    parallel replicates

--- fishing/sb3/replicates.R
+++ fishing/sb3/replicates.R
@@ -2,82 +2,84 @@
 library(reticulate)
 library(tidyverse)
 
-
-## Python dependencies, via reticulate
-gym         <- import ("gym")
-gym_fishing <- import("gym_fishing")
-sb3         <- import ("stable_baselines3")
-os          <- import ("os")
-torch       <- import ("torch")
-np          <- import ("numpy")
-
-
 ## NB: Most integers must be explicitly typed as such (e.g. `1L` for `1`)
-
 ##  Turn CUDA off for reproducibility, if necessary.
 Sys.setenv("CUDA_VISIBLE_DEVICES" = "0")
 
-## Set seeds
-torch$manual_seed(12345L)
-np$random$seed(12345L)
 
-## Initialize our environment
-ENV <- "fishing-v0"
-env <- gym$make(ENV, n_actions = 100L, sigma = 0.05) # with some process noise
-
-train <- function(env, algo = "SAC"){
-  init_model <- sb3[[algo]]
-  model <- init_model('MlpPolicy', env, verbose=1L)
-  model$learn(total_timesteps=200000L)
+result_path <- function(prefix, i, dir = "results"){
+  file.path(dir, paste0(prefix, "_", algo, "_", i, ".csv"))
 }
 
+## self-contained iterable, for parallel execution
+train <- function(i = 0, 
+                  ENV = "fishing-v1", 
+                  algo = "SAC", 
+                  steps = 200000L, 
+                  tensorboard_log="/var/log/tensorboard",
+                  dir = "results", 
+                  ...){
+  # Load python modules
+  gym_fishing <- import("gym_fishing")
+  gym         <- import("gym")
+  sb3         <- import ("stable_baselines3")
+  torch       <- import("torch")
+  np          <- import("numpy")
+  
+  torch$manual_seed(12345L)
+  np$random$seed(12345L)
+  
+  env <- gym$make(ENV, ...)
+  init_model <- sb3[[algo]]
+  model <- init_model('MlpPolicy', 
+                      env, 
+                      verbose=0L,
+                      tensorboard_log=tensorboard_log)
+  model$learn(total_timesteps=as.integer(steps))
+
+  ## save models
+  dir.create("results")
+  lapply(seq_along(models), function(i){
+    models[[i]]$save(file.path(dir, paste0(algo, i)))
+  })
+  
+  
+  sims <- env$simulate(model, reps=50L)
+  policy <- env$policyfn(model, reps=500L)
+  
+  ## Save simulations and policyfn
+  readr::write_csv(sims, result_path("sims", i, dir))
+  readr::write_csv(policy, result_path("policy", i, dir))
+  
+  list(sims, policy)
+}
 
 ## Here we go.  Sit tight, this is gonna take a while!
-models <- lapply(1:5, function(i) train(env, "SAC"))
+## We do this in parallel sessions
+library(future.apply)
+plan("multisession")
+res <- future.apply::future_lapply(1:5, train, sigma = 0.05)
+  
 
-## save models
-dir.create("results")
-lapply(seq_along(models), function(i){
-  models[[i]]$save(paste0("results/SAC", i))
-})
+## Load results
+df <- vroom::vroom(result_path("sims", 1:5))
+policy <- vroom::vroom(result_path("policy", 1:5))
 
-## Load models
-#models <- lapply(paste0("results/SAC", 1:5), sb3$SAC$load)
-
-
-## simulate models
-df <- map_dfr(models, env$simulate, reps=50L, .id = "model")
-
-policy <- map_dfr(models, env$policyfn, reps=500L, .id = "model")
-policy$action <- unlist(policy$action)
-
-policy %>% group_by(state, model) %>% summarize(action = mean(action)) %>%
-  ggplot(aes(state, action, col=model)) + geom_line()
-
+## Some fun plotting
 
 low <- function(x) quantile(x, probs = seq(0,1,by=0.05))[2]
 high <- function(x) quantile(x, probs = seq(0,1,by=0.05))[20]
 
 p2 <- policy %>% 
   group_by(state) %>% 
-  summarise(mean_action = mean(action), low = low(action), high = high(action)) 
-
+  summarise(mean_action = mean(action), 
+            low = low(action), 
+            high = high(action)) 
 p2 %>%
   ggplot(aes(state, mean_action)) + 
   geom_line() +
   geom_ribbon(aes(ymin = low, ymax = high), alpha = 0.2)
 ggsave("results/SAC-policy.png")
-
-## Evaluate model over n replicates
-reward <- 
-  map_dfr(models, function(model){
-    reward <- sb3$common$evaluation$evaluate_policy(model, env, n_eval_episodes=50L)
-    reward <- data.frame(mean = reward[[1]], sd = reward[[2]])
-    reward
-    },
-    .id = "model"
-  )
-    ##
 
 ## repair NAs
 as.na <- function(x){
