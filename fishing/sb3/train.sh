0a1ddd89a5fc7fbfdeac118897946969362ef3f9
Carl <cboettig@gmail.com>
2020-11-19 21:09:35 +0000

    update tuning script example

--- fishing/sb3/train.sh
+++ fishing/sb3/train.sh
@@ -3,7 +3,9 @@
 python zoo_train.py \
   --algo sac --env fishing-v1 -n 300000 -optimize \
   --n-trials 1000 --n-jobs 3 --sampler random --pruner median \
-  --tensorboard-log /var/log/tensorboard/train_gpu &
+  --tensorboard-log /var/log/tensorboard/train_gpu \
+  --storage sqlite:///tuning_data.db \
+  --study-name sac-fishingv1
 
 CUDA_VISIBLE_DEVICES="" python zoo_train.py \
   --algo sac --env fishing-v1 -n 300000 -optimize \
