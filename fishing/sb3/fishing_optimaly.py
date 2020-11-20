e62ff25b015f4e9d9ea91ee44076d07c16c9c9e8
Carl <cboettig@gmail.com>
2020-11-20 17:16:17 +0000

    'robot commit before running script'

--- fishing/sb3/fishing_optimaly.py
+++ fishing/sb3/fishing_optimaly.py
@@ -1,83 +1,28 @@
-
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt 
-import gym 
+import gym
 import gym_fishing
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from leaderboard import leaderboard, hash_url
+import os
+url = hash_url(os.path.basename(__file__)) # get hash URL at start of execution
 
+ENV = "fishing-v1" # can do cts or discrete
+env = gym.make(ENV)
+model = escapement(env)
+df = env.simulate(model, reps=10)
+env.plot(df, "results/escapement.png")
+policy = env.policyfn(model, reps=10)
+env.plot(policy, "results/escapement-policy.png")
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
+leaderboard.leaderboard("ESC", ENV, mean_reward, std_reward, url)
 
-# ## Continuous action space 
-cts_env = gym.make('fishing-v1')
-
-
-def simul(environment) :
-  time_step = np.array([environment.reset()])
-  episode_return = np.array([0.0])
-  output = np.zeros(shape = (1000, 4))
-
-  for it in range(1000):
-    action = max(time_step[0] - 0.5, np.array([0.0]) )
-    time_step = environment.step(action)
-    episode_return += action
-    output[it] = (it, time_step[0], action, episode_return)
-  return output 
-
-
-out=simul(cts_env)
-
-fig, axs = plt.subplots(3,1)
-axs[0].plot(out[:,0], out[:,1])
-axs[0].set_ylabel('state')
-axs[1].plot(out[:,0], out[:,2])
-axs[1].set_ylabel('action')
-axs[2].plot(out[:,0], out[:,3])
-axs[2].set_ylabel('reward')
-
-fig.tight_layout()
-plt.savefig("results/optimal_v1.png")
-
-
-
-# ## Discrete Action Space
-
-env = gym.make('fishing-v0')
-def simulate(environment) :
-    time_step = environment.reset()
-    episode_return = 0.0
-    output = np.zeros(shape = (1000, 4))
-    action = 0
-    time_step = environment.step(action)
-    episode_return += time_step[1]
-    output[0] = (0, time_step[0], action, episode_return)
-
-    for it in range(1,1000):
-        if time_step[1] <= max(time_step[0] - 0.5, 0.0) :
-            action = 1
-        if time_step[1] > max(time_step[0] - 0.5, 0.0) :
-            action = 2
-
-        time_step = environment.step(action)
-        episode_return += time_step[1]
-        output[it] = (it, time_step[0], action, episode_return)
-    return output    
-
-time_step = env.reset()
-time_step = env.step(1)
-time_step
-
-out = simulate(env)
-
-
-
-fig, axs = plt.subplots(3,1)
-axs[0].plot(out[:,0], out[:,1])
-axs[0].set_ylabel('state')
-axs[1].plot(out[:,0], out[:,2])
-axs[1].set_ylabel('action')
-axs[2].plot(out[:,0], out[:,3])
-axs[2].set_ylabel('reward')
-
-fig.tight_layout()
-plt.savefig("results/optimal_v0.png")
-
+########################################################
+model = msy(env)
+## simulate and plot results
+df = env.simulate(model, reps=10)
+env.plot(df, "results/msy.png")
+policy = env.policyfn(model, reps=10)
+env.plot(policy, "results/msy-policy.png")
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
+leaderboard.leaderboard("MSY", ENV, mean_reward, std_reward, url)
 
