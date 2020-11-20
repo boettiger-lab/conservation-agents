c2e4a091efd40231c5e281b24fde1ab4aad35865
Carl <cboettig@gmail.com>
2020-11-20 20:46:39 +0000

    recompute leaderboard

--- fishing/sb3/train.sh
+++ fishing/sb3/train.sh
@@ -12,7 +12,8 @@ python zoo_train.py \
   --storage sqlite:///tuning.db \
   --study-name td3-fishingv1
   
-python zoo_train.py   --algo a2c --env fishing-v1 -n 300000 -optimize   --n-trials 400 --n-jobs 3 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name a2c-fishingv1
+python zoo_train.py   --algo a2c --env fishing-v1 -n 300000 -optimize   --n-trials 400 --n-jobs 4 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name a2c-fishingv1
 
 python zoo_train.py   --algo ppo --env fishing-v1 -n 300000 -optimize   --n-trials 100 --n-jobs 4 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name ppo-fishingv1
 
+python zoo_train.py   --algo ddpg --env fishing-v1 -n 300000 -optimize   --n-trials 100 --n-jobs 4 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name ddpg-fishingv1
