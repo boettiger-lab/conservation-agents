fcf98856c0ba16550f457f9e2f1fe985902daeee
Carl <cboettig@gmail.com>
2020-11-20 00:17:09 +0000

    tuning

--- fishing/sb3/train.sh
+++ fishing/sb3/train.sh
@@ -3,11 +3,11 @@
 python zoo_train.py \
   --algo sac --env fishing-v1 -n 300000 -optimize \
   --n-trials 1000 --n-jobs 3 --sampler random --pruner median \
-  --tensorboard-log /var/log/tensorboard/train_gpu \
-  --storage sqlite:///tuning_data.db \
+  --storage sqlite:///tuning.db \
   --study-name sac-fishingv1
 
-CUDA_VISIBLE_DEVICES="" python zoo_train.py \
-  --algo sac --env fishing-v1 -n 300000 -optimize \
-  --n-trials 1000 --n-jobs 3 --sampler random --pruner median \
-  --tensorboard-log /var/log/tensorboard/train &
+
+  
+python zoo_train.py   --algo a2c --env fishing-v1 -n 300000 -optimize   --n-trials 400 --n-jobs 3 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name a2c-fishingv1
+
+python zoo_train.py   --algo ppo --env fishing-v1 -n 300000 -optimize   --n-trials 100 --n-jobs 4 --sampler random --pruner median   --storage sqlite:///tuning.db   --study-name ppo-fishingv1
