5f6d7a53a66b988489ca3de6c30024d0d7a73114
Carl <cboettig@gmail.com>
2020-11-21 00:50:32 +0000

    'robot commit before running script'

--- fishing/sb3/compute_leaderboard.py
+++ fishing/sb3/compute_leaderboard.py
@@ -5,6 +5,7 @@ from stable_baselines3 import SAC, TD3, A2C, PPO, DDPG, DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 from leaderboard import leaderboard, hash_url
 import os
+from torch import nn as nn
 
 #file = os.path.basename(__file__)
 file = "compute_leaderboard.py"
@@ -56,9 +57,27 @@ env.plot(df, "results/ppo.png")
 ## A2C ######################################################################
 
 # FIXME load best tuned parameters first...
-# Trial 328 finished with value: 8.025644302368164 and parameters: {'gamma': 0.98, 'normalize_advantage': False, 'max_grad_norm': 0.3, 'use_rms_prop': True, 'gae_lambda': 0.98, 'n_steps': 16, 'lr_schedule': 'linear', 'lr': 0.03490204662520112, 'ent_coef': 0.00026525398345043097, 'vf_coef': 0.18060066335808234, 'log_std_init': -1.1353269076856574, 'ortho_init': True, 'net_arch': 'medium', 'activation_fn': 'relu'}. 
+# Trial 328 finished with value: 8.025644302368164 and parameters: 
+hyper = {'gamma': 0.98, 'normalize_advantage': False, 'max_grad_norm': 0.3, 'use_rms_prop': True, 'gae_lambda': 0.98, 'n_steps': 16,
+         'lr_schedule': 'linear', 'lr': 0.03490204662520112, 
+         'ent_coef': 0.00026525398345043097, 'vf_coef': 0.18060066335808234, 
+         'log_std_init': -1.1353269076856574, 'ortho_init': True, 'net_arch': 'medium', 'activation_fn': 'relu'}.
+policy_kwargs = dict(log_std_init=hyper["log_std_init"],
+                     ortho_init = hyper["ortho_init"],
+                     activation_fn = nn.ReLU,
+                     net_arch=[256, 256])
 
-model = A2C('MlpPolicy', env, verbose=0, tensorboard_log=tensorboard_log)
+model = A2C('MlpPolicy', env, verbose=0, tensorboard_log=tensorboard_log,
+            gamma = hyper["gamma"],
+            learning_rate = hyper["lr"],
+            normalize_advantage = hyper["normalize_advantage"],
+            gae_lambda = hyper["gae_lambda"],
+            n_steps = hyper["n_steps"],
+            ent_coef = hyper["ent_coef"],
+            vf_coef = hyper["vf_coef"],
+            
+            policy_kwargs = policy_kwargs
+            )
 model.learn(total_timesteps=300000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
 leaderboard("A2C", ENV, mean_reward, std_reward, url)
@@ -100,7 +119,7 @@ hyper = {'gamma': 0.99,
          'tau': 0.005, 
          'log_std_init': -0.3072998266889968, 
          'net_arch': 'medium'}
-policy_kwargs = dict(log_std_init=-3.67, net_arch=[256, 256])
+policy_kwargs = dict(log_std_init=hyper["log_std_init"], net_arch=[256, 256])
 
 model = SAC('MlpPolicy', 
             env, verbose=0, 
@@ -137,6 +156,7 @@ env.plot(df, "results/sac.png")
 hyper = {'gamma': 0.95, 'lr': 0.001737794384065678, 'batch_size': 256, 'buffer_size': 1000000, 'episodic': True, 'noise_type': None, 'noise_std': 0.260511264163344, 'net_arch': 'medium'}
 policy_kwargs = dict(net_arch=[256, 256])
 
+## WTF are noise_type, noise_std, (action noise? policy noise?) and episodic?  
 model = TD3('MlpPolicy', 
             env, 
             verbose=0, 
@@ -144,8 +164,8 @@ model = TD3('MlpPolicy',
             learning_rate = hyper['lr'],
             batch_size = hyper['batch_size'],            
             buffer_size = hyper['buffer_size'],
-            noise_type = hyper['noise_type'],
-            noise_std = hyper['noise_std'],
+#            noise_type = hyper['noise_type'],
+#            noise_std = hyper['noise_std'],
             policy_kwargs=policy_kwargs,
             tensorboard_log=tensorboard_log)
 model.learn(total_timesteps=300000)
