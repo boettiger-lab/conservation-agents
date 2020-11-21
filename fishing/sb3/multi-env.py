41023002f8b7f73d3267f081a9f3c89aec509d61
Carl <cboettig@gmail.com>
2020-11-21 06:47:06 +0000

    'robot commit before running script'

--- fishing/sb3/multi-env.py
+++ fishing/sb3/multi-env.py
@@ -6,31 +6,81 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from leaderboard import leaderboard, hash_url
 import os
 
-#file = os.path.basename(__file__)
-file = "multi_env.py"
+file = os.path.basename(__file__)
+#file = "multi_env.py"
 url = hash_url(file) # get hash URL at start of execution
-tensorboard_log="/var/log/tensorboard/leaderboard"
+tensorboard_log="/var/log/tensorboard/multi-env"
 
 
 ENV = "fishing-v1"    
 env = gym.make(ENV)
-
-
-## A2C ######################################################################
-
-# FIXME load best tuned parameters first...
-# Trial 328 finished with value: 8.025644302368164 and parameters: {'gamma': 0.98, 'normalize_advantage': False, 'max_grad_norm': 0.3, 'use_rms_prop': True, 'gae_lambda': 0.98, 'n_steps': 16, 'lr_schedule': 'linear', 'lr': 0.03490204662520112, 'ent_coef': 0.00026525398345043097, 'vf_coef': 0.18060066335808234, 'log_std_init': -1.1353269076856574, 'ortho_init': True, 'net_arch': 'medium', 'activation_fn': 'relu'}. 
-
 model = A2C('MlpPolicy', env, verbose=0, tensorboard_log=tensorboard_log)
+model.set_env(env)
+
 model.learn(total_timesteps=300000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
-leaderboard("A2C", ENV, mean_reward, std_reward, url)
 print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+ENV = "fishing-v1"    
+env = gym.make(ENV, r = 0.1)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+ENV = "fishing-v1"    
+env = gym.make(ENV)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+
+
+ENV = "fishing-v1"    
+env = gym.make(ENV, r = 0.1)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+
+ENV = "fishing-v2"    
+env = gym.make(ENV, C = 0.4)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+ENV = "fishing-v2"    
+env = gym.make(ENV, C = 0.2)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+ENV = "fishing-v2"    
+env = gym.make(ENV, C = 0.1, sigma = 0.1)
+model.set_env(env)
+model.learn(total_timesteps=300000)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+
+
+
+
+
 
 ## simulate and plot results for reference
 df = env.simulate(model, reps=10)
-env.plot(df, "results/a2c.png")
+env.plot(df, "results/a2c-multi.png")
 #policy = env.policyfn(model, reps=10)
 #env.plot(policy, "results/a2c-policy.png")
 
-
+model.save("results/a2c-multi")
