b7ba251891381502e37f26e82fdd0dc19b78a1af
Carl <cboettig@gmail.com>
2020-11-25 20:48:53 +0000

    'robot commit before running script'

--- fishing/sb3/multi-env.py
+++ fishing/sb3/multi-env.py
@@ -21,6 +21,10 @@ env = gym.make(ENV, C = 0.2)
 model.set_env(env)
 model.learn(total_timesteps=500000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+# Rescale score against optimum solution in this environment
+opt = escapement(env)
+opt_reward, std_reward = evaluate_policy(opt, env, n_eval_episodes=100)
+mean_reward = mean_reward / opt_reward; std_reward = std_reward / opt_reward   
 print("algo:", "A2C2", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
 ## simulate and plot results for reference
 df = env.simulate(model, reps=10)
@@ -31,6 +35,10 @@ env = gym.make(ENV, C = 0.1)
 model.set_env(env)
 model.learn(total_timesteps=500000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+# Rescale score against optimum solution in this environment
+opt = escapement(env)
+opt_reward, std_reward = evaluate_policy(opt, env, n_eval_episodes=100)
+mean_reward = mean_reward / opt_reward; std_reward = std_reward / opt_reward   
 print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
 df = env.simulate(model, reps=10)
 env.plot(df, "results/serial-eval2.png")
@@ -41,6 +49,10 @@ env = gym.make(ENV)
 model.set_env(env)
 model.learn(total_timesteps=500000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+# Rescale score against optimum solution in this environment
+opt = escapement(env)
+opt_reward, std_reward = evaluate_policy(opt, env, n_eval_episodes=100)
+mean_reward = mean_reward / opt_reward; std_reward = std_reward / opt_reward   
 print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
 df = env.simulate(model, reps=10)
 env.plot(df, "results/serial-eval3.png")
@@ -50,6 +62,10 @@ env = gym.make(ENV, r = 0.1)
 model.set_env(env)
 model.learn(total_timesteps=500000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+# Rescale score against optimum solution in this environment
+opt = escapement(env)
+opt_reward, std_reward = evaluate_policy(opt, env, n_eval_episodes=100)
+mean_reward = mean_reward / opt_reward; std_reward = std_reward / opt_reward   
 print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
 df = env.simulate(model, reps=10)
 env.plot(df, "results/serial-eval4.png")
