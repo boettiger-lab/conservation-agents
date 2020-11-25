18154a08553a5b36640d73870e3562ccc5041c6b
Carl <cboettig@gmail.com>
2020-11-25 04:07:42 +0000

    'robot commit before running script'

--- fishing/sb3/A2C.py
+++ fishing/sb3/A2C.py
@@ -24,3 +24,37 @@ print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_rewa
 df = env.simulate(model, reps=10)
 env.plot(df, "results/A2C-v4.png")
 
+
+ENV = "fishing-v1"    
+env = gym.make(ENV, r = 0.1)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+## simulate and plot results for reference
+df = env.simulate(model, reps=10)
+env.plot(df, "results/A2C_1.png")
+
+
+ENV = "fishing-v1"    
+env = gym.make(ENV, r = 0.3)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+## simulate and plot results for reference
+df = env.simulate(model, reps=10)
+env.plot(df, "results/A2C_2.png")
+
+ENV = "fishing-v1"    
+env = gym.make(ENV, r = 0.4)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+## simulate and plot results for reference
+df = env.simulate(model, reps=10)
+env.plot(df, "results/A2C_3.png")
+
+
+ENV = "fishing-v2"    
+env = gym.make(ENV, C = 0.1)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
+## simulate and plot results for reference
+df = env.simulate(model, reps=10)
+env.plot(df, "results/A2C_4.png")
