f38503a8d366744f1c140b750efd3833ea84fe38
Carl <cboettig@gmail.com>
2020-12-04 18:17:07 +0000

    'robot commit before running script'

--- fishing/sb3/benchmark.py
+++ fishing/sb3/benchmark.py
@@ -17,7 +17,7 @@ tensorboard_log="/var/log/tensorboard/benchmark"
 ENV = "fishing-v1"    
 env = gym.make(ENV, r = 0.05)
 model = A2C('MlpPolicy', env, verbose=0)
-model.learn(total_timesteps=300000)
+model.learn(total_timesteps=3000000)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
 print("algo:", "A2C", "env:", ENV, "mean reward:", mean_reward, "std:", std_reward)
 ## simulate and plot results for reference
