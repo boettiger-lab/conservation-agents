d391431a4cc3c55147cf28602d289679d64be6ed
Carl <cboettig@gmail.com>
2020-12-18 04:56:28 +0000

    experiments

--- conservation/tune.sh
+++ conservation/tune.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES="" python ../tuning/zoo_train.py   --algo a2c --env conservation-v5 -n 100000 -optimize \
-  --n-trials 100 --n-jobs 4 --sampler random --pruner median \
-  --storage sqlite:///tuning.db   --study-name a2c-cons-v5 &
+python ../tuning/zoo_train.py   --algo a2c --env conservation-v2 -n 100000 -optimize \
+  --n-trials 10 --n-jobs 4 --sampler random --pruner median \
+  --storage sqlite:///tuning.db   --study-name a2c-cons-v2 &
 
 CUDA_VISIBLE_DEVICES="" python ../tuning/zoo_train.py   --algo ppo --env conservation-v5 -n 100000 -optimize \
   --n-trials 100 --n-jobs 4 --sampler random --pruner median \
