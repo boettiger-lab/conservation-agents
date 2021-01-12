cc33c03f348bc0a04ee81cfe49af46b339923a74
Carl <cboettig@gmail.com>
2021-01-12 00:39:02 +0000

    v6 pars

--- conservation/A2C.py
+++ conservation/A2C.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append("../tuning")
 from parse_hyperparameters import best_hyperpars, custom_eval, a2c
 
-env_id = "conservation-v5"
+env_id = "conservation-v6"
 algo="a2c"
 outdir = "results"
 total_timesteps = 3000000
@@ -16,22 +16,23 @@ tensorboard_log="/var/log/tensorboard/single"
 # NB: See utils/hyperparams_opt.py for what is and isn't tuned for each model!
 # Manual -- A2C defaults
 hyper = {
-"params_activation_fn": "tanh",
-"params_ent_coef": 0.0,
-"params_gae_lambda": 1.0,
-"params_gamma": 0.99,
-"params_log_std_init": 0.0,
-"params_lr": 0.0007,
-"params_lr_schedule": "linear",
-"params_max_grad_norm": 0.5,
-"params_n_steps": 5,
-"params_net_arch": "small",
-"params_normalize_advantage": False,
-"params_ortho_init": True,
-"params_use_rms_prop": True,
-"params_vf_coef":  0.5,
-"value": 0 # only in logs
-}
+'params_activation_fn':                              "relu",
+"params_ent_coef":                               0.00387734,
+'params_gae_lambda':                                   0.99,
+'params_gamma':                                       0.995,
+"params_log_std_init":                              -3.0706,
+"params_lr":                                      0.0123908,
+"params_lr_schedule":                              "linear",
+"params_max_grad_norm":                                   2,
+"params_n_steps":                                       128,
+"params_net_arch":                                 "medium",
+"params_normalize_advantage":                         False,
+"params_ortho_init":                                   True,
+"params_use_rms_prop":                                 True,
+"params_vf_coef":                                  0.972425,
+'value':  51.2587}
+
+
 
 # Alternately, load tuned values  from log dir
 #hyper = best_hyperpars(log_dir, env_id, algo)
@@ -41,7 +42,7 @@ use_sde = True
 n_envs = 4
 
 env = make_vec_env(env_id, n_envs = n_envs, seed = seed)
-model = a2c(env, hyper, 'MlpPolicy', verbose = verbose, tensorboard_log = tensorboard_log, seed = seed, use_sde = use_sde)
+model = a2c(env, hyper, 'MlpPolicy', verbose = verbose, tensorboard_log = tensorboard_log, seed = seed, use_sde = use_sde, device="cpu")
 model.learn(total_timesteps = total_timesteps)
 custom_eval(model, env_id, algo, seed = seed, outdir = outdir, value = hyper["value"])
 
--- conservation/SAC.py
+++ conservation/SAC.py
@@ -6,12 +6,12 @@ sys.path.append("../tuning")
 from parse_hyperparameters import best_hyperpars, custom_eval, sac
 
 import gym
-import gym_fishing
+import gym_conservation
 
-env_id = "conservation-v5"
+env_id = "conservation-v6"
 algo="sac"
 outdir = "results"
-total_timesteps = 300000
+total_timesteps = 1e7
 verbose = 0
 seed = 0
 tensorboard_log="/var/log/tensorboard/single"
@@ -19,15 +19,15 @@ tensorboard_log="/var/log/tensorboard/single"
 # NB: See utils/hyperparams_opt.py for what is and isn't tuned for each model!
 # Manual -- A2C defaults
 hyper = {
-"params_lr": 0.0003,
-"params_gamma": 0.99,
-"params_batch_size": 256,
-"params_buffer_size": 1000000,
-"params_learning_starts": 100,
-"params_train_freq": 1,
-"params_tau": 0.005,
-"params_log_std_init": -3,
-"params_net_arch": "small",
+'params_batch_size':                                  256,
+'params_buffer_size':                               10000,
+'params_gamma':                                    0.9999,
+'params_learning_starts':                           10000,
+'params_log_std_init':                           0.268563,
+'params_lr':                                  0.000292858,
+'params_net_arch':                               'medium',
+'params_tau':                                       0.001,
+'params_train_freq':                                   64,
 "value": 0 # only in logs
 }
 
@@ -43,12 +43,12 @@ use_sde=False
 sde_sample_freq=- 1
 use_sde_at_warmup=False
 # Alternately, load tuned values  from log dir
-#hyper = best_hyperpars(log_dir, env_id, algo)
+#hyper = best_hyperpars("logs", "conservation-v5", algo)
 # Override defaults
 use_sde = True
 
 
-env = gym.make(env_id, Tmax=1000)
+env = gym.make(env_id)
 model = sac(env, hyper, 'MlpPolicy', verbose = verbose, tensorboard_log = tensorboard_log, seed = seed, use_sde = use_sde)
 model.learn(total_timesteps = total_timesteps)
 custom_eval(model, env_id, algo, seed = seed, outdir = outdir, value = hyper["value"])
