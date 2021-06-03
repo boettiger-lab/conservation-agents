788498660b8a1964d128646b901503b86b12718e
Carl <cboettig@gmail.com>
2021-06-03 23:13:35 +0000

    more runs

--- fishing/tune.sh
+++ fishing/tune.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES="1" python ../tuning/zoo_train.py \
-  --algo sac --env fishing-v1 -n 300000 -optimize \
-  --n-trials 1 --n-jobs 1 --sampler tpe --pruner median \
+  --algo sac --env fishing-v1 -n 600000 -optimize \
+  --n-trials 40 --n-jobs 1 --sampler tpe --pruner median \
   --storage sqlite:///tuning.db \
   --study-name sac-fishingv1
 
 CUDA_VISIBLE_DEVICES="1" python ../tuning/zoo_train.py \
   --algo td3 --env fishing-v1 -n 300000 -optimize \
-  --n-trials 100 --n-jobs 1 --sampler tpe --pruner median \
+  --n-trials 20 --n-jobs 1 --sampler tpe --pruner median \
   --storage sqlite:///tuning.db \
   --study-name td3-fishingv1
   
 CUDA_VISIBLE_DEVICES="1" python ../tuning/zoo_train.py   --algo a2c --env fishing-v1 -n 300000 -optimize \
-  --n-trials 200 --n-jobs 4  \
-  --storage sqlite:///tuning.db   --study-name a2c-fishingv1
+  --n-trials 200 --storage sqlite:///tuning.db   --study-name a2c-fishingv1
 
 CUDA_VISIBLE_DEVICES="1" python ../tuning/zoo_train.py   --algo ppo --env fishing-v1 -n 300000 -optimize  \
-  --n-trials 200 --n-jobs 4 --sampler tpe --pruner median \
-  --storage sqlite:///tuning.db   --study-name ppo-fishing-v1
+  --n-trials 20 --storage sqlite:///tuning.db   --study-name ppo-fishing-v1
 
 CUDA_VISIBLE_DEVICES="1" python ../tuning/zoo_train.py   --algo ddpg --env fishing-v1 -n 300000 -optimize \
   --n-trials 100 --n-jobs 4 --sampler tpe --pruner median   \
